0ff07d912ff7d4cb8d1ba8f5a767cc10c48af46c
boluwatifejames <123778863+boluwatifejames@users.noreply.github.com>
2024-09-11 15:09:51 +0100

    Update storeRoutes.js

--- routes/storeRoutes.js
+++ routes/storeRoutes.js
@@ -8,11 +8,8 @@ router.get('/all',  storeController.getAllStores);
 router.get('/:storeId/info', storeController.getStoreInfo);
 
 
-<<<<<<< HEAD
+
 router.delete('/delete/:storeId', storeController.deleteStoreById);  // Delete store by ID
 router.put('/edit/:storeId', storeController.editStoreById);//Edit store by ID 
-=======
 
-
->>>>>>> c5dedcb30e66a1068ad8dc52b60d039136ddc3e0
 module.exports = router;
