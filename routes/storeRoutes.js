2bd6b4b5592a7e52554160046d8797351e06cb36
boluwatifejames <bosingwa100@gmail.com>
2024-09-16 15:00:51 +0100

    updated store functionalities

--- routes/storeRoutes.js
+++ routes/storeRoutes.js
@@ -6,6 +6,9 @@ const storeController = require('../controllers/storeController');
 router.post('/create', storeController.createStore);
 router.get('/all',  storeController.getAllStores);
 router.get('/:storeId/info', storeController.getStoreInfo);
+router.get('/search', storeController.searchStore);
+router.get('/filter', storeController.filterByLocation);
+
 
 
 //CREATE A TEMPORARY USER TO TEST
