205554dc0397aaa5d27361bc0830771f28839f5e
Augustine Ofor <100804858+Iupac120@users.noreply.github.com>
2024-09-23 09:29:41 +0100

    updated client url

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const {  swaggerUi,swaggerSpec} = require("./swagger")
 
 
 const corsOptions = {
-    origin: process.env.CLIENT_URL,
+    origin: "https://skilltopims.com",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     allowedHeaders: "Content-Type,Authorization",
