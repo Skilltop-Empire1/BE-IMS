80f563ee20166bbcdf99ebd1516f5122cd344f1a
boluwatifejames <bosingwa100@gmail.com>
2024-09-23 15:21:36 +0100

    added profile picture logic

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -3,7 +3,11 @@ const express = require("express");
 
 //create express router
 const router = express.Router();
+const profileController = require('../controllers/profileController');
 
+router.post('/upload', profileController.uploadProfilePic);
+
+router.get('/:userId', profileController.getProfilePic);
 
 
 module.exports = router;
